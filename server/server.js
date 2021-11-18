b4cc837c1c4c9383962f0ab975dcb681313c1ef8
James M Kim <jamesk@Jamess-MBP.lan>
2021-11-17 19:35:51 -0500

    added path

--- server/server.js
+++ server/server.js
@@ -2,3 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', express.static(path.resolve(__dirname, '../client/public')));
+
+app.listen(PORT, console.log(`Listening on PORT: ${PORT}...`));
+
+module.exports = app;
