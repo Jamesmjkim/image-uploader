5ddc5b97b0fa52631bedeb684798be7ce9a9f669
James M Kim <jamesk@Jamess-MBP.lan>
2021-11-28 23:22:59 -0500

    updates

--- client/src/Containers/MainContainer.jsx
+++ client/src/Containers/MainContainer.jsx
@@ -1,51 +1,86 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 
-const MainContainer = () => {
-    const mainStyles = {
-        width: '35rem',
-        height: '37rem',
+class MainContainer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={};
+        
+        this.onFileLoad = this.onFileLoad.bind(this);
     }
-    const subStyle = {
-        width: '27.5rem',
-        height: '20rem',
-        display: 'flex',
-        justifyContent: 'center'
-    }
-    const [mountain, setMountain] = useState('')
-    useEffect(() => {
-        // let mountain;
-        fetch('/static/sampleImage.svg').then(image => {
-            setMountain(image.url);
-            // console.log(image.url)
+    componentDidMount() {
+
+        fetch('http://localhost:3000/static/sampleImage.png',{
+            mode: 'cors'
+        }).then(image => {
+            this.setState({image: image.url});
         })
+    }
+    onFileLoad(e) {
+        const fileTypes ={
+            'image/jpeg' : true,
+            'image/png' : true
+        }
+        const file = e.currentTarget.files[0];
+        // console.log(file.type)
+        if (!file) return;
+        else if(fileTypes[file.type]) {
+            this.setState({image:URL.createObjectURL(file)});
+            fetch('http://localhost:3000/uploadImage',{
+                method: 'POST',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': file.type
+                },
+                body: file
+            }).then(res => console.log(res))
+        }
+        
 
-    },[])
 
-    console.log(mountain)
-
-    return (
-        <div className="position-absolute top-50 start-50 translate-middle">
-            <div className='container text-center'>
-                <div className="card" style={mainStyles}>
-                    <div className="card-body">
-                        <h5 className="card-title m-3">Upload Your Image</h5>
-                        <p className="card-text m-3">File should be Jpeg, Png,... </p>
-
-                        <div className="card text-dark bg-light mb-3 mx-auto" style={subStyle}>
-                            <div className="text-center">
-                                <img src={mountain} className="rounded" alt="Sample" />
+    }
+    
+    render() {
+            return (
+                <div className="position-absolute top-50 start-50 translate-middle">
+                    <div className='container text-center'>
+                        <div className="card" style={{
+                                    width: '35rem',
+                                     height: '37rem',
+                                }}>
+                            <div className="card-body">
+                                <h5 className="card-title m-3">Upload Your Image</h5>
+                                <p className="card-text m-3">File should be Jpeg, Png,... </p>
+        
+                                <div className="card text-dark bg-light mb-3 mx-auto" style={{
+                                    width: '27.5rem',
+                                    height: '20rem',
+                                    display: 'flex',
+                                    justifyContent: 'center'
+                                }}>
+                                    <div className="text-center">
+                                        <img src={this.state.image} className="rounded" alt="Sample" style={{
+                                            maxWidth: '25rem'
+                                        }} />
+                                        {/* <p className='m-5 mb-4'>Drag & Drop your image here</p> */}
+                                    </div>
+                                </div>
+        
+                                {/* <button type="button" className="btn btn-primary m-3">Choose a file</button> */}
+                                <div className="input-group mb-3">
+                                    <input type="file" className="form-control" id="inputGroupFile02" onChange={this.onFileLoad}/>
+                                    <label className="input-group-text" htmlFor="inputGroupFile02">Upload</label>
+                                </div>
                             </div>
                         </div>
-
-                        <p className="card-text m-3">Or</p>
-                        <button type="button" className="btn btn-primary m-3">Choose a file</button>
                     </div>
                 </div>
-            </div>
-        </div>
+        
+            )
 
-    )
+    }
+    
+    
 }
 
 export default MainContainer;
