881835b03ddea8a4d2ba84bce6bbf5f917ff091f
James M Kim <jamesk@Jamess-MBP.lan>
2021-11-28 20:04:55 -0500

    changed concept

--- server/server.js
+++ server/server.js
@@ -4,13 +4,19 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/static', express.static(path.resolve(__dirname, '../client/public')));
+app.use(
+  '/static',
+  express.static(path.resolve(__dirname, './../client/public'))
+);
+
+app.post('/upload', (req, res) => {});
 
 app.listen(PORT, console.log(`Listening on PORT: ${PORT}...`));
 
